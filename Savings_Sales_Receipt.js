1319269320e134907f961507e71bf4a75129cbe5
Adrian-Samuel <31516988+Adrian-Samuel@users.noreply.github.com>
2018-08-26 10:59:24 -0400

    Create Savings_Sales_Receipt.js

---
+++ Savings_Sales_Receipt.js
@@ -0,0 +1,49 @@
+/* Code to display total Savings on the Sales Receipt
+Savings = MSRP - Sales Price + Discount Value
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+
+let str = window.location.href; 
+ let new_num = str.replace(/[\s\S]+?([\d]+)\.html[\s\S]+/ig, "$1");  
+ let radPatt = /[\s\S]+\bAccount\/\b(\d{1,})[\s\S]+/ig; 
+ let RAD = str.replace(radPatt, "$1"); 
+ let domain = window.location.host; 
+ let thankYou =  document.querySelector('.thankyou'); 
+fetch(`https://${domain}/API/Account/${RAD}/DisplayTemplate/Sale/${new_num}.json`, {
+    credentials: "same-origin"}) 
+    .then((response) => response.json()) 
+    .then((data) => { 
+if (typeof data.Sale.SaleLines.SaleLine.length  == "undefined") {
+    let amountPaid = data.Sale.calcTotal;
+     let defaultPrice = data.Sale.SaleLines.SaleLine.Item.Prices.ItemPrice[0].amount;  
+     let MSRP = data.Sale.SaleLines.SaleLine.Item.Prices.ItemPrice[1].amount;
+     let quantity = data.Sale.SaleLines.SaleLine.unitQuantity
+   if((amountPaid/quantity) != MSRP || amountPaid/quantity) != defaultPrice) {
+       let promotion = (defaultPrice * quantity) - amountPaid; 
+       let savings = ((MSRP - defaultPrice )* quantity ) + promotion;
+       thankYou.insertAdjacentHTML('afterend', `<span style="position:absolute; left: 409px; font-size: 9pt;">YOU SAVE $${savings.toFixed(2)} TODAY!</span>`);
+    } 
+}
+else {
+    let itemTotal = data.Sale.SaleLines.SaleLine; 
+    let savings = 0
+    for(let i = 0; i < itemTotal.length; i++ ) {
+    	let quantity = data.Sale.SaleLines.SaleLine[i].unitQuantity;
+
+    let defaultPrice = data.Sale.SaleLines.SaleLine[i].Item.Prices.ItemPrice[0].amount;
+    let MSRP = data.Sale.SaleLines.SaleLine[i].Item.Prices.ItemPrice[1].amount;
+    let amountPaid = data.Sale.SaleLines.SaleLine[i].calcTotal;
+    if((amountPaid/quantity) != MSRP || amountPaid/quantity) != defaultPrice) {
+        let promotion = (defaultPrice * quantity) - amountPaid;
+        savings = savings + ((MSRP - defaultPrice) * quantity) + promotion;
+    
+        }
+    } 
+    thankYou.insertAdjacentHTML('afterend', `<span style="position:absolute; left: 409px; font-size: 9pt;">YOU SAVE $${savings.toFixed(2)} TODAY!</span>`);
+
+}
+
+});
+
+});
