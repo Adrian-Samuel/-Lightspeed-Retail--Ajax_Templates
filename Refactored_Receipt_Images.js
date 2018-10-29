a3debf6bd5a31e2b471bf0172cf76d75d4de505a
Adrian-Samuel <31516988+Adrian-Samuel@users.noreply.github.com>
2018-10-28 23:02:55 -0400

    Create Refactored_Receipt_Images.js

---
+++ Refactored_Receipt_Images.js
@@ -0,0 +1,61 @@
+/* Rewrote Images on Receipt Script using Async/Await along with other functional methods */
+(async () => {
+    try {
+        const numbers = window.location.href.match(/\d+/g);
+        const RAD = numbers[0];
+        const saleID = numbers[1];
+        const domain = window.location.host;
+        const visibleSales = document.querySelectorAll('.sale.lines tbody .description > div:nth-child(1)');
+        const itemTitle = document.querySelector('.sale.lines th.description');
+        itemTitle.insertAdjacentHTML('afterend', `<th> Images </th>`);
+
+
+        let sale_request = await fetch(`https://${domain}/API/Account/${RAD}/DisplayTemplate/Sale/${saleID}.json`, {
+            credentials: 'same-origin'
+        })
+
+        let sale_result = await sale_request.json();
+
+        let saleList = sale_result.Sale.SaleLines.SaleLine;
+
+        if (!Array.isArray(sale_result.Sale.SaleLines.SaleLine)) {
+            saleList = [sale_result.Sale.SaleLines.SaleLine];
+        }
+        saleList.map(async (x, i) => {
+
+            if (x.hasOwnProperty('Item')) {
+                let itemID = x.Item.itemID;
+
+                let image_request = await fetch(`https://${domain}/API/Account/${RAD}/Item/${itemID}/Image.json`, {
+                    credentials: 'same-origin'
+                });
+
+                let image_result = await image_request.json();
+
+                let itemImages = image_result.Image;
+                if (image_result["@attributes"].count != 0) {
+
+                    if (!Array.isArray(image_result.Image)) {
+                        itemImages = [image_result.Image];
+                    }
+
+                    console.log(itemImages);
+                    let url = `${itemImages[0].baseImageURL}${itemImages[0].publicID}`;
+                    console.log(`${url} for item ${i +1}`);
+                    visibleSales[i].parentElement.insertAdjacentHTML('afterend', `<td style="border-bottom: 1px solid black;"><img style="width: 100px; "src ="${itemImages[0].baseImageURL}${itemImages[0].publicID}"></td>`)
+
+                } else {
+                    visibleSales[i].parentElement.insertAdjacentHTML('afterend', `<td style="width: 100px; height: 100px; border-bottom: 1px solid black;" ></td>`)
+                }
+            } else {
+                visibleSales[i].parentElement.insertAdjacentHTML('afterend', `<td style="width: 100px; height:100px; border-bottom: 1px solid black;"></td>`)
+            }
+
+        });
+
+    } catch (err) {
+        console.log(err);
+    } finally {
+        window.print();
+    }
+})();
